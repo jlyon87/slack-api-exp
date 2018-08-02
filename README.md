85277658b30cbd59eef17c95a79a0a54d7b01021
jlyonas <jlyon@acumensolutions.com>
2018-08-02 15:14:35 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# slack-api-exp
+
+> Experimenting with the slack-api module.
+
+[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard)
+
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
---
+++ src/index.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const config = require('../config/slack.config')
+const Slack = require('slack-api').promisify()
+
+// console.log(Slack)
+console.log('config', config)
+
+Slack.auth.test({ token: config.client_id })
+  .then(data => {
+    console.log('auth', data)
+  })
+  .catch(Slack.errors.SlackError, function (error) {
+    console.log('Slack did not like what you did: ' + error.message)
+  })
+  .catch(Slack.errors.CommunicationError, function (error) {
+    console.error('Error communicating with Slack. ' + error.message)
+  })
+  .catch(Slack.errors.SlackServiceError, function (error) {
+    console.error('Error communicating with Slack. ' + error.message)
+    // To get error details
+    console.error(error.errorDetails)
+  })
+
+Slack.api.test(config)
+  .then(data => {
+    console.log('api', data)
+  })
+  .catch(Slack.errors.SlackError, function (error) {
+    console.log('Slack did not like what you did: ' + error.message)
+  })
+  .catch(Slack.errors.CommunicationError, function (error) {
+    console.error('Error communicating with Slack. ' + error.message)
+  })
+  .catch(Slack.errors.SlackServiceError, function (error) {
+    console.error('Error communicating with Slack. ' + error.message)
+    // To get error details
+    console.error(error.errorDetails)
+  })
+
+Slack.search.all({ token: config.client_id, query: 'has:[:upside_down_face:]' })
+  .then(data => {
+    console.log('search', data)
+  })
+  .catch(Slack.errors.SlackError, function (error) {
+    console.log('Slack did not like what you did: ' + error.message)
+  })
+  .catch(Slack.errors.CommunicationError, function (error) {
+    console.error('Error communicating with Slack. ' + error.message)
+  })
+  .catch(Slack.errors.SlackServiceError, function (error) {
+    console.error('Error communicating with Slack. ' + error.message)
+    // To get error details
+    console.error(error.errorDetails)
+  })
